8a786a4e17739b2f3b3f94ead4869af7240070c5
Snerble <conor.groenenberg@icloud.com>
2019-05-15 20:02:37 +0200

    Added Unix support

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 USER_AGENT = 'e621browser0.1 (by CubeGamer6)'
 BASE_URL = 'https://e621.net'
 
-DOWNLOADS = 'downloads\\'
+DOWNLOADS = 'downloads/'
 METADATA  = DOWNLOADS + 'meta.ini'
 
-RESOURCES        = "res\\"
-TEMP_SUBD_DIR    = '\\E621 DL Cache'
+RESOURCES        = "res/"
+TEMP_SUBD_DIR    = '/E621 DL Cache'
 CONCURRENT_LIMIT = 30
 
 ICON_16x16 = RESOURCES + "16x16.png"
 ICON_32x32 = RESOURCES + "32x32.png"
+ICON_128x128 = RESOURCES + "128x128.png"
 
 META_TAGS = ( 'artist', 'children', 'created_at', 'fav_count', 'parent_id', 'rating', 'score', 'sources', 'tags' )
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
 import config
+
+import os
+import tempfile
 
 from interface import searchInterface
 
@@ -21,42 +25,28 @@ class Program:
         The remaining kwargs will be passed to the pyglet.window.Window() constructor. See the
         pyglet.window.Window() documentation for more info.
         """
-        _config = Config(sample_buffers=kwargs.pop("sample_buffers", 1), samples=kwargs.pop("samples", 4))
+        screen = canvas.get_display().get_default_screen()
+        try:
+            _kwargs = dict(sample_buffers=kwargs.pop("sample_buffers", 1), samples=kwargs.pop("samples", 4))
+            _config = Config(**_kwargs)
+            _config = screen.get_best_config(_config)
+        except:
+            print(f"Configuration {_kwargs} failed;. Falling back to default config...")
+        finally:
+            _config = screen.get_best_config()
         self.window = window.Window(width, height, resizable=True, visible=kwargs.pop("visible", True), config=_config, **kwargs)
-        self.window.set_icon(image.load(config.ICON_16x16), image.load(config.ICON_32x32))
+        self.window.set_icon(image.load(config.ICON_16x16), image.load(config.ICON_32x32), image.load(config.ICON_128x128))
         if caption: self.window.set_caption(caption)
 
-        @self.window.event
-        def on_draw(*args):
-            self.on_draw(*args)
-        @self.window.event
-        def on_resize(*args):
-            self.on_resize(*args)
-        @self.window.event
-        def on_key_press(*args):
-            self.on_key_press(*args)
-
-        self.__draw_hooks = set()
-        self.__resize_hooks = set()
-        self.__keypress_hooks = set()
-
-    def add_draw_hook(self, func): self.__draw_hooks.add(func)
-    def remove_draw_hook(self, func): self.__draw_hooks.remove(func)
-    def on_draw(self):
-        self.window.clear()
-        for hook in self.__draw_hooks: hook()
-
-    def add_resize_hook(self, func): self.__resize_hooks.add(func)
-    def remove_resize_hook(self, func): self.__resize_hooks.remove(func)
-    def on_resize(self, width, height):
-        for hook in self.__resize_hooks: hook(width, height)
-
-    def add_keypress_hook(self, func): self.__keypress_hooks.add(func)
-    def remove_keypress_hook(self, func): self.__keypress_hooks.remove(func)
-    def on_key_press(self, symbol, modifiers):
-        for hook in self.__keypress_hooks: hook(symbol, modifiers)
-
 if __name__ == "__main__":
+    if not os.path.exists(tempfile.gettempdir() + config.TEMP_SUBD_DIR):
+        os.mkdir(tempfile.gettempdir() + config.TEMP_SUBD_DIR)
+    for f in os.listdir(tempfile.gettempdir() + config.TEMP_SUBD_DIR):
+        try:
+            f = tempfile.gettempdir() + config.TEMP_SUBD_DIR + '/' + f
+            os.remove(f)
+            print('Removed', f)
+        except: pass
     program = Program("E621 Browser")
     searchInterface.init(program)
     app.run()
